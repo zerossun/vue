457d5ea5bf22beed759746915d166a7c9fe6bb0c
zerossun <wonsunyoung0703@gmail.com>
2024-01-16 01:59:11 +0900

    20240116

--- vue_study/src/components/TheWelcome.vue
+++ vue_study/src/components/TheWelcome.vue
@@ -1,88 +1,91 @@
-<script setup>
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
-</script>
-
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+<div>
+    <select v-model="item">
+        <option :value="itme.v" :key="i" v-for="(itme,i) in optoins">{{ itme.t }}</option>
+    </select>   
+    <table>
+        <thead>
+            <tr>
+                <th>제품명</th>
+                <th>제품가격</th>
+                <th>배송비</th>
+                <th>제품카테고리</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr :key="i" v-for="(item,i) in productList">
+                <td>{{ item.product_name }}</td>
+                <td>{{ item.price }}</td>
+                <td>{{ item.delivery }}</td>
+                <td>{{ item.category }}</td>
+            </tr>
+            
+        </tbody>
+    </table>
+    <!-- 
+        [넣고 싶은 기능]
+        1. 버튼을 클릭했을 때 해당 버튼에 active 클래스 들어가게
+        2. 다른 버튼을 클릭하면 반대로
+        3. 이거를 삼항연산자로 최대한 코드를 줄이고 싶다.></div>
+     -->
+    <p>이상 버튼 해결</p>
+    <button class="normal" :class="isDone? 'active' : 'default'" @click="btnOut" >버튼 A</button>
+    <button class="normal" :class="isDone? 'active' : 'default'" @click="btnOn">버튼 B</button>
+    <input v-if="show"/>
+</div>
 </template>
+<script>
+
+export default{ 
+    name:'',
+    components:{},
+    data(){
+        return{
+            optoins:[
+                {v:'02', t:'서울'},
+                {v:'21', t:'부산'},
+                {v:'064', t:'제주'},
+            ],
+            item:'02',
+            productList:[
+                {product_name:'기계식 키보드', price:85000, delivery:5000, category:'전자기기'},
+                {product_name:'마우스', price:25000, delivery:2500, category:'전자기기'},
+            ],
+            show:false,
+            active:'active',
+            isDone :false
+        };
+    },
+    setup(){},
+    created(){},
+    mounted(){},
+    unmounted(){},
+    methods:{
+        btnOn(e){
+            this.show = true;
+        },
+        btnOut(e){
+            this.show = false;
+        }
+   }
+}
+</script>
+<style>
+.normal{
+display: flex;
+width: 158px;
+padding: 16px 24px;
+flex-direction: column;
+align-items: center;
+gap: 8px;
+border-radius: 10px;
+}
+.default{
+border: 1px solid var(--system-guide-color-grey_line, #E5E5E5);
+background: var(--system-guide-color-white, #FFF);}
+.active{
+border: 1px solid var(--system-guide-color-red_main, #E30613);
+background: var(--system-guide-color-red_light, #FFE4E6);
+color: #E30613;
+}
+</style>
--- vue_study/src/main.js
+++ vue_study/src/main.js
@@ -1,3 +1,5 @@
+// 빌드를 시작할 때 가장 처음 불러오는 진입지점
+// 해당 파일에서 app파일을 불러와 vue 객체를 생성하고 app 바인딩
 import './assets/main.css'
 
 import { createApp } from 'vue'
